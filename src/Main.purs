d53fe5540abe34165377b80614934576477cefb6
bouzuya <m@bouzuya.net>
2019-03-03 15:01:16 +0900

    add readTextFromStream

--- src/Main.purs
+++ src/Main.purs
@@ -5,10 +5,27 @@ module Main
 import Data.Array as Array
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
+import Effect.Aff (Aff)
+import Effect.Aff as Aff
 import Effect.Class.Console as Console
+import Effect.Ref as Ref
+import Node.Encoding as Encoding
 import Node.Process as Process
+import Node.Stream (Readable)
+import Node.Stream as Stream
 import Options as Options
-import Prelude (Unit, bind, (<$>))
+import Prelude (Unit, append, bind, discard, mempty, pure, (<$>))
+
+readTextFromStream :: Readable () -> Aff String
+readTextFromStream r = Aff.makeAff \callback -> do
+  ref <- Ref.new mempty
+  Stream.onDataString r Encoding.UTF8 \s -> do
+    buffer <- Ref.read ref
+    Ref.write (append buffer s) ref
+  Stream.onEnd r do
+    buffer <- Ref.read ref
+    callback (pure buffer)
+  pure mempty -- canceler
 
 main :: Effect Unit
 main = do
@@ -18,4 +35,7 @@ main = do
     Just options ->
       if options.help
         then Console.log "HELP"
-        else Console.logShow args
+        else Aff.launchAff_ do
+          Console.logShow args
+          input <- readTextFromStream Process.stdin
+          Console.log input
