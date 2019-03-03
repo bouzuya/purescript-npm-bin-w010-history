2f16627e0135d4ff1a92486ab9a1c0c468887830
bouzuya <m@bouzuya.net>
2019-03-03 14:19:10 +0900

    show help

--- src/Main.purs
+++ src/Main.purs
@@ -3,12 +3,19 @@ module Main
   ) where
 
 import Data.Array as Array
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Class.Console as Console
 import Node.Process as Process
+import Options as Options
 import Prelude (Unit, bind, (<$>))
 
 main :: Effect Unit
 main = do
   args <- (Array.drop 2) <$> Process.argv
-  Console.logShow args
+  case Options.parse args of
+    Nothing -> Console.log "invalid option"
+    Just options ->
+      if options.help
+        then Console.log "HELP"
+        else Console.logShow args
